14b33b3fec25067be3ad6e8e8fb5e087790b6adc
pdzhagriev <pavloid21@gmail.com>
2020-03-18 02:02:01 -0700

    view and upload config file

--- awx/ui/client/src/deploy/deploy.controller.js
+++ awx/ui/client/src/deploy/deploy.controller.js
@@ -1,34 +1,79 @@
 export default [
-    "$rootScope",
-    "$scope",
-    "$location",
-    "ConfigService",
-    "lastPath",
-    "$http",
-    "Wait",
-    "Uploader",
-    ($rootScope, $scope, $location, ConfigService, lastPath, $http, Wait, Uploader) => {
+  "$rootScope",
+  "$scope",
+  "$location",
+  "ConfigService",
+  "lastPath",
+  "$http",
+  "Wait",
+  "Uploader",
+  (
+    $rootScope,
+    $scope,
+    $location,
+    ConfigService,
+    lastPath,
+    $http,
+    Wait,
+    Uploader
+  ) => {
+    $scope.fileText = "";
+    $scope.lineNumbers = [];
 
-        $scope.setDomain = () => {
-            console.log('fired', $scope.domain);
-        }
+    let handleFiles = file => {
+      const reader = new FileReader();
+      reader.onload = event => {
+        const file = event.target.result;
+        const allLines = file.split(/\r\n|\n/);
+        allLines.forEach((line, index) => {
+          $scope.fileText += `${line}\n`;
+          $scope.lineNumbers.push(index + 1);
+        });
+        $scope.showPopup = true;
+        let tArea = document.getElementById("listing");
+        let numBlock = document.getElementById("nums");
+        tArea.addEventListener("scroll", event => {
+          numBlock.scrollTop = tArea.scrollTop;
+        });
+      };
 
-        $scope.setConfig = (files) => {
-            console.log(files[0])
-            var r = Uploader.upload('/deploy/uploads/', files[0]);
-            r.then(
-                function(r) {
-                // success
-                $scope.showPopup = true;
-                },
-                function(r) {
-                // failure
-                console.warn(r)
-                });
-        }
+      reader.onerror = event => {
+        alert(event.target.error.name);
+      };
 
-        $scope.closePopup = () => {
-            $scope.showPopup = false;
+      reader.readAsText(file);
+    };
+
+    $scope.setDomain = () => {
+      console.log("fired", $scope.domain);
+    };
+
+    $scope.setConfig = files => {
+      $scope.fileObj = files[0];
+      handleFiles(files[0]);
+    };
+
+    $scope.uploadConfig = () => {
+      Wait("start");
+      let r = Uploader.upload("/deploy/uploads/", $scope.fileObj);
+      r.then(
+        function(r) {
+          // success
+          $scope.showPopup = false;
+          Wait("stop");
+        },
+        function(r) {
+          // failure
+          Wait("stop");
+          console.warn(r);
         }
-    }
+      );
+    };
+
+    $scope.closePopup = () => {
+      $scope.fileText = "";
+      $scope.lineNumbers.length = 0;
+      $scope.showPopup = false;
+    };
+  }
 ];
